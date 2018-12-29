2844c912f3315f8acc6c37883f95ce90482f42fa
Johan Ferret <johan.ferret1@gmail.com>
2018-12-29 15:33:44 +0100

    Evolutionary generation / modification ideas

---
+++ imgproc/evolve.py
@@ -0,0 +1,13 @@
+# evolutionary process ideas :
+#
+# 1 - 
+# mutation = application of transfo from (partition +) alter, morph
+# genotype = phenotype = image(s)
+# fitness = user-defined based on phenotype
+#
+# 2 -
+# mutation = adding / subtracting / modifying from transfo sequence
+# genotype = transfo sequence
+# phenotype = image(s) resulting from cascading transfos in sequence
+# fitness = user-defined based on phenotype
+#
